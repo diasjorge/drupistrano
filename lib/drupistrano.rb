e290dd9264d5b3acba61bad4cecb34553e93ce91
Jorge Dias <jorge@mrdias.com>
2011-02-09 14:20:38 +0100

    Added support for ISPConfig. Using relative symlinks

--- lib/drupistrano.rb
+++ lib/drupistrano.rb
@@ -8,6 +8,7 @@ Capistrano::Configuration.instance.load do
   _cset(:site_uri)   { "default" }
   _cset(:drush_path) { "drush" }
   _cset(:drush_cmd)  { "#{drush_path} --uri=#{site_uri}" }
+  _cset(:ispconfig)  { false }
 
   namespace :deploy do
     task :start do ; end
@@ -31,7 +32,7 @@ Capistrano::Configuration.instance.load do
           rsync -avz files/ #{shared_path}/files && \
           rm -rf files; \
         fi; \
-        ln -nsf #{shared_path}/files .
+        cd #{release_path}/sites/default && ln -nsf ../../../../shared/files/ .
       CMD
     end
 
@@ -57,7 +58,40 @@ Capistrano::Configuration.instance.load do
 
     desc "Execute database updates"
     task :migrate, :roles => :db do
-      run "cd #{current_path} && #{drush_cmd} -y updatedb"
+      run "cd #{current_release} && #{drush_cmd} -y updatedb"
+    end
+
+    # Rewrite symlink task to add support for ISPConfig
+    desc <<-DESC
+      Updates the symlink to the most recently deployed version. Capistrano works \
+      by putting each new release of your application in its own directory. When \
+      you deploy a new version, this task's job is to update the `current' symlink \
+      to point at the new version. You will rarely need to call this task \
+      directly; instead, use the `deploy' task (which performs a complete \
+      deploy, including `restart') or the 'update' task (which does everything \
+      except `restart').
+    DESC
+    task :symlink, :except => { :no_release => true } do
+      if ispconfig
+        previous_normalized = previous_release.gsub(deploy_to, '').gsub(/^\//,'')
+        latest_normalized = latest_release.gsub(deploy_to, '').gsub(/^\//,'')
+
+        rollback_cmd = "rm -f #{current_path}; cd #{deploy_to} && ln -s #{previous_normalized} current; true"
+        symlink_cmd = "rm -f #{current_path}; cd #{deploy_to} && ln -s #{latest_normalized} current"
+      else
+        rollback_cmd = "rm -f #{current_path}; ln -s #{previous_release} #{current_path}; true"
+        symlink_cmd = "rm -f #{current_path} && ln -s #{latest_release} #{current_path}"
+      end
+
+      on_rollback do
+        if previous_release
+          run rollback_cmd
+        else
+          logger.important "no previous release to rollback to, rollback of symlink skipped"
+        end
+      end
+
+      run symlink_cmd
     end
   end
 
